f2afd2c0c93473aadf9f4fad1870e48fe6a07ea0
Sam Jordan <jordan12sam@gmail.com>
2022-03-13 12:34:50 +0000

    added functional UI elements

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from datetime import datetime
 from PIL import Image, ImageTk
 import numpy as np
 import tkinter as tk
-import threading
 import serial
 import time
-import math
 
 import os
 os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
@@ -14,7 +12,8 @@ os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import cv2
 from scipy.optimize import linear_sum_assignment
 
-#class for object tracking
+# tracker object
+# tracks objects across frames
 class Tracker:
     def __init__(self):
         #store info on objects in a dictionary
@@ -22,10 +21,17 @@ class Tracker:
         self.previous_objects = {}
         self.objects = {}
 
-        #info for the tracked object
-        self.object_id = -1
-        self.object_pos = (0, 0)
+        # info for the target object
+        self.tracking = False
+        self.target_id = 57
+        self.target_pos = (0, 0)
 
+    # toggle the tracking param
+    # when True, the motors will follow the target object
+    def track(self):
+        self.tracking = not self.tracking
+
+    # assign ids to the objects in a new frame
     def update(self, detection):
         #number of frames missing objects are remembered for
         MEMORY = 100
@@ -91,46 +97,186 @@ class Tracker:
 
         self.previous_objects = self.objects
 
+    # clear the memory of the tracker
     def clear(self):
         self.objects = {}
         self.previous_objects = {}
 
-class Gui(tk.Frame):
-    def __init__(self):
+# gui object
+# handles GUI
+class Gui():
+
+    def overlay_command(self):
+        self.overlay = not self.overlay
+    
+    def record_command(self):
+        self.recording = not self.recording
+    
+    def screenshot_command(self):
+        file_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
+        #cv2.imwrite(f".\Screenshots\main_{file_time}.png", main_frame)
+        cv2.imwrite(f".\screenshots\guide_{file_time}.png", self.full_image)
+        print(f"Saving screenchot as \"{file_time}\"")
+    
+    def motor_command(self, motor):
+        try:
+            move_motor(motor, int(self.steps_entry.get()))
+        except ValueError as e:
+            print(e)
+    
+    def __init__(self, tracker):
+        self.tracker = tracker
+
         self.root = tk.Tk()
-        self.panel = None
         self.open = True
+
+        self.pady = 5
+
+        self.full_image = None
+
+        self.video_frame = None
+        self.video_width = 4
+
+        # video options
+        self.video_label = tk.Label(self.root, text="Video Options")
+        self.video_label.grid(row=0, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.overlay = True
+        self.overlay_button = tk.Button(self.root, text="Toggle Overlay", command=self.overlay_command)
+        self.overlay_button.grid(row=1, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.recording = False
+        self.record_button = tk.Button(self.root, text="Toggle Video", command=self.record_command)
+        self.record_button.grid(row=1, column=self.video_width+2, padx=10, pady=self.pady)
+
+        self.screenshot = False
+        self.screenshot_button = tk.Button(self.root, text="Screenshot", command=self.screenshot_command)
+        self.screenshot_button.grid(row=1, column=self.video_width+3, padx=10, pady=self.pady)
+
+        # tracking options
+        self.tracking_label = tk.Label(self.root, text="Tracking Options")
+        self.tracking_label.grid(row=3, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.clear_button = tk.Button(self.root, text="Reset Object IDs", command=self.tracker.clear)
+        self.clear_button.grid(row=4, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.tracking = False
+        self.tracking_button = tk.Button(self.root, text="Toggle Tracking Mode", command=self.tracker.track)
+        self.tracking_button.grid(row=4, column=self.video_width+2, padx=10, pady=self.pady)
+
+        self.tracking_id_input = tk.Entry(self.root, justify="right")
+        self.tracking_id_input.insert(tk.END, "Enter Tracking ID")
+        self.tracking_id_input.grid(row=4, column=self.video_width+3, padx=10, pady=self.pady)
+
+        # manual control
+        self.tracking_label = tk.Label(self.root, text="Manual Control")
+        self.tracking_label.grid(row=6, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.up = tk.Button(self.root, text="Move Azimuth", command=lambda: self.motor_command("az"))
+        self.up.grid(row=7, column=self.video_width+1, padx=10, pady=self.pady)
+
+        self.left = tk.Button(self.root, text="Move Elevation", command=lambda: self.motor_command("elv"))
+        self.left.grid(row=7, column=self.video_width+2, padx=10, pady=self.pady)
+
+        self.steps_entry = tk.Entry(self.root, justify="right")
+        self.steps_entry.insert(tk.END, "Enter Steps")
+        self.steps_entry.grid(row=7, column=self.video_width+3, padx=10, pady=self.pady)
 
         self.root.wm_title("Tracking Control Panel")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def update_video(self, frame):
+    def draw_overlay(self, loop_time):
+        # get frame dimensions
+        (y, x) = self.full_image.shape[:2]
+
+        # centre circle in the guidescope
+        cv2.circle(self.full_image, (x//2, y//2), 5, (0, 0, 255), -1)
+
+        # outline main camera FOV in the guidescope
+        # main FOV 0.3056deg x 0.2196deg
+        # guide FOV 1.91deg x 1.07deg
+        # thus the main FOV is bounded by the centre 20.5% x 16% of the guide frame
+        (h, w) = (int(0.205*y), int(0.16*x))
+        cv2.rectangle(self.full_image, ((x-w)//2, (y-h)//2), ((x+w)//2, (y+h)//2), (0, 0, 255), 1)
+
+        # write tracking id on frame
+        if self.tracker.tracking:
+            self.full_image = cv2.putText(self.full_image, f"Tracking", 
+                                        (5, y-25), 
+                                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+        self.full_image = cv2.putText(self.full_image, f"Tracking id: {self.tracker.target_id}", 
+                                    (5, y-10), 
+                                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+
+        for id, obj in self.tracker.objects.items():
+            x, y, width, height, dx, dy, mem = obj
+
+            # if this is the object we are tracking
+            # then change the colour from blue to red
+            # else leave it as blue
+            if id == self.tracker.target_id and self.tracker.tracking:
+                self.tracker.target_pos = (x, y)
+                text_colour = (0, 0, 255)
+                box_colour = (0, 0, 255)
+            else:
+                text_colour = (255, 0, 0)
+                box_colour = (0, 255, 0)
+
+            if not width*height == 0:
+                cv2.putText(self.full_image, str(id), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, text_colour, 2)
+                cv2.rectangle(self.full_image, (x, y), (x + width, y + height), box_colour, 1)
+
+        # update fps counter
+        loop_time = time.perf_counter() - loop_time
+        cv2.putText(self.full_image, f"{1/loop_time:02.2f} FPS", (5, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+
+        if self.recording:
+            # write 'recording' in bottom left of frame to notify user the video feed is being recorded
+            cv2.putText(self.full_image, f"Recording", (5, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+
+    def update_video(self, frame, loop_time):
+        # save raw input image
+        # if enabled, add the overlay
+        self.full_image = frame
+        if self.overlay:
+            self.draw_overlay(loop_time)
+
+        # downsize resolution of video
         # OpenCV represents images in BGR order; however PIL
         # represents images in RGB order, so we need to swap
         # the channels, then convert to PIL and ImageTk format
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(self.full_image, (1024, 576))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(image)
         image = ImageTk.PhotoImage(image)
 
         # if the panel is not None, we need to initialize it
-        if self.panel is None:
+        if self.video_frame is None:
             print("Initialise GUI")
-            self.panel = tk.Label(image=image)
-            self.panel.image = image
-            self.panel.pack(side="left", padx=10, pady=10)
+            self.video_frame = tk.Label(image=image)
+            self.video_frame.image = image
+            self.video_frame.grid(row=0, column=0, rowspan=32, columnspan=self.video_width, padx=10, pady=10)
 
         # otherwise, simply update the panel
         else:
-            self.panel.configure(image=image)
-            self.panel.image = image
+            self.video_frame.configure(image=image)
+            self.video_frame.image = image
     
+    def update_tracking_id(self):
+        try:
+            self.tracker.target_id = int(self.tracking_id_input.get())
+        except ValueError:
+            self.tracker.target_id = 0
+
     def on_close(self):
         print("Exiting...")
         self.open = not self.open
         self.root.destroy()
         self.root.quit()
 
-def send_motor(motor, steps):
+# sends a movement command to a given motor
+# movement based on given steps
+def move_motor(motor, steps):
     # commands are enclosed with <>
     # a/b for azimuth/elevation motor respectively
     # +/- for clockwise/anticlockwise respectively
@@ -162,9 +308,10 @@ def send_motor(motor, steps):
     #then close the serial communications
 
     cmd = cmd.encode("utf-8")
-    ser.write(cmd)
-    time.sleep(1)
+    #ser.write(cmd)
 
+# detects objects in the frame
+# returns a list of object coordinates and sizes
 def get_bounding_rectangles(frame):
     #greyscale and blur both the current and previous frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -202,9 +349,6 @@ def main():
     # main setup
     print("Starting main...")
 
-    # GUI setup
-    gui = Gui()
-
     # define camera dimensions
     guide_dimensions = (1920, 1080)
     main_dimensions = (3856, 2764)
@@ -239,12 +383,9 @@ def main():
 
     # initialise euclidean distance tracker object
     tracker = Tracker()
-    # initialise object tracking as false
-    tracking = False
-    # initialise camera recording as false
-    recording = False
-    # initialise object tracking overlay as true
-    overlay = True
+
+    # GUI setup
+    gui = Gui(tracker)
 
     # main tracking loop
     # run until user closes GUI
@@ -269,117 +410,25 @@ def main():
         # get a list of objects with ids
         tracker.update(object_bounding_rectangles)
 
+        # update tracking id
+        gui.update_tracking_id()
+
         # move the motors if we are tracking an object
-        if tracking:
-            x, y = tracker.object_pos
+        if tracker.tracking:
+            x, y = tracker.target_pos
             x_mov = guide_dimensions[0]/2 - x
             y_mov = guide_dimensions[1]/2 - y
             #print(f"{x_mov}, {y_mov}")
 
-
-        # user keyboard interactions
-        key = cv2.waitKey(1)
-
-        # exit with e
-        if key == ord('e'):
-            print("Exiting...")
-            break
-
-        # save screenshot with [space]
-        if key == ord(' '):
-            file_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
-            #cv2.imwrite(f".\Screenshots\main_{file_time}.png", main_frame)
-            cv2.imwrite(f".\Screenshots\guide_{file_time}.png", guide_frame)
-            print(f"Saving screenchot as \"{file_time}\"")
-        
-        # toggle tracking with t
-        if key == ord('t'):
-            tracking = not tracking
-
-        # toggle recording with r
-        if key == ord('r'):
-            recording = not recording
-
-        # toggle overlay with o
-        if key == ord('o'):
-            overlay = not overlay
-
-        # clear ids with c
-        if key == ord('c'):
-            tracker.clear()
-
-        # increment tracking id with w, decrement with s
-        # increment by 10 with q, decrement by 10 with a
-        if key == ord('w'):
-            tracker.object_id += 1
-        elif key == ord('s'):
-            tracker.object_id -= 1
-            if tracker.object_id < 0:
-                tracker.object_id = 0
-
-        if key == ord('q'):
-            tracker.object_id += 10
-        elif key == ord('a'):
-            tracker.object_id -= 10
-            if tracker.object_id < 0:
-                tracker.object_id = 0
-
-        if overlay:
-            for id, obj in tracker.objects.items():
-                x, y, width, height, dx, dy, mem = obj
-
-                # if this is the object we are tracking
-                # then change the colour from blue to red
-                # else leave it as blue
-                if id == tracker.object_id and tracking:
-                    tracker.object_pos = (x, y)
-                    text_colour = (0, 0, 255)
-                    box_colour = (0, 0, 255)
-                else:
-                    text_colour = (255, 0, 0)
-                    box_colour = (0, 255, 0)
-
-                if not width*height == 0:
-                    cv2.putText(guide_frame, str(id), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, text_colour, 2)
-                    cv2.rectangle(guide_frame, (x, y), (x + width, y + height), box_colour, 1)
-
-            # centre circle in the guidescope
-            (x, y) = guide_dimensions
-            cv2.circle(guide_frame, (x//2, y//2), 5, (0, 0, 255), -1)
-
-            # outline main camera FOV in the guidescope
-            # main FOV 0.3056deg x 0.2196deg
-            # guide FOV 1.91deg x 1.07deg
-            # thus the main FOV is bounded by the centre 20.5% x 16% of the guide frame
-            (h, w) = (int(0.205*y), int(0.16*x))
-            cv2.rectangle(guide_frame, ((x-w)//2, (y-h)//2), ((x+w)//2, (y+h)//2), (0, 0, 255), 1)
-
-            # write tracking id on frame
-            if tracking:
-                guide_frame = cv2.putText(guide_frame, f"Tracking", 
-                                            (5, guide_dimensions[1]-25), 
-                                            cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-            guide_frame = cv2.putText(guide_frame, f"Tracking id: {tracker.object_id}", 
-                                        (5, guide_dimensions[1]-10), 
-                                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-
-            # update fps counter
-            loop_time = time.perf_counter() - loop_time
-            cv2.putText(guide_frame, f"{1/loop_time:02.2f} FPS", (5, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-
-            if recording:
-                # write 'recording' in bottom left of frame to notify user the video feed is being recorded
-                cv2.putText(guide_frame, f"Recording", (5, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-
-        if recording:
-            # save frame to video file
-            guide_out.write(guide_frame)
-            #main_out.write(cv2.resize(main_frame, guide_dimensions))
-
-        # downsize the resolution before showing to the user
-        gui_frame = cv2.resize(guide_frame, (1024, 576))
-        gui.update_video(gui_frame)
+        # send video to gui
+        gui.update_video(guide_frame, loop_time)
         gui.root.update()
+
+        # save frame
+        if gui.recording:
+            # save frame to video file
+            guide_out.write(gui.full_image)
+            #main_out.write(cv2.resize(main_frame, guide_dimensions))
 
     guide_out.release()
     #main_out.release()
